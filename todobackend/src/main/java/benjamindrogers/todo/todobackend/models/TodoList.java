906dced8b271cdb31f4a9dd7681d2f218562a298
Benjamin Rogers <benjamindoghertyrogers@gmail.com>
2024-09-25 00:02:22 +0200

    Remove default items added for testing

--- todobackend/src/main/java/benjamindrogers/todo/todobackend/models/TodoList.java
+++ todobackend/src/main/java/benjamindrogers/todo/todobackend/models/TodoList.java
@@ -1,14 +1,13 @@
 package benjamindrogers.todo.todobackend.models;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class TodoList {
     
     private ArrayList<String> list;
 
     public TodoList() {
-        this.list = new ArrayList<String>(Arrays.asList("test1", "test2"));
+        this.list = new ArrayList<String>();
     }
 
     public ArrayList<String> getList() {
