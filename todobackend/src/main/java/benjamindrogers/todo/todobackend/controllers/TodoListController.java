93dcda0d5b13b5c1d2ec7e3a0d8fe6d9f91d5281
Benjamin Rogers <benjamindoghertyrogers@gmail.com>
2024-09-24 22:25:46 +0200

    Add endpoint for adding items

--- todobackend/src/main/java/benjamindrogers/todo/todobackend/controllers/TodoListController.java
+++ todobackend/src/main/java/benjamindrogers/todo/todobackend/controllers/TodoListController.java
@@ -3,6 +3,7 @@ package benjamindrogers.todo.todobackend.controllers;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,10 +12,17 @@ import benjamindrogers.todo.todobackend.models.TodoList;
 @RestController
 @RequestMapping("/todo")
 public class TodoListController {
-    
+    private TodoList todoList = new TodoList();
+
     @GetMapping
     public ResponseEntity<TodoList> getTodoList() {
-        return new ResponseEntity<TodoList>(new TodoList(), HttpStatus.OK);
+        return new ResponseEntity<TodoList>(todoList, HttpStatus.OK);
     }
-    
+
+    @PostMapping
+    public ResponseEntity<TodoList> addItem() {
+        todoList.addItem();
+        return new ResponseEntity<TodoList>(todoList, HttpStatus.OK);
+    }
+
 }
